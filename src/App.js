3f4fe1a46208dcf160b0263595cc33cdcdf9d999
Aloysius Paredes <aloysius.paredes@gmail.com>
2020-01-03 16:37:49 -0600

    basic screen layout, csv reader, flexbox

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import CSVReader from "react-csv-reader";
+import { Column, Row } from "simple-flexbox";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    userName: "",
+    firstName: "",
+    lastName: "",
+    isUploaded: false,
+    isValid: false
+  };
+
+  logName = () => {
+    // do whatever with the names ... let's just log them here
+    console.log(this.state.userName);
+    console.log(this.state.firstName);
+    console.log(this.state.lastName);
+  };
+
+  handleUserNameInput = e => {
+    this.setState({ userName: e.target.value });
+  };
+  handleFirstNameInput = e => {
+    this.setState({ firstName: e.target.value });
+  };
+  handleLastNameInput = e => {
+    this.setState({ lastName: e.target.value });
+  };
+
+  csvUpload = data => {
+    console.log(data);
+    this.setState({
+      isUploaded: true
+    });
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <Column flexGrow={1} style={{ "padding-top": "25px" }}>
+        <Row horizontal="center">
+          <img
+            src="https://branch.io/img/logo-dark.svg"
+            style={{ height: "100px", "padding-right": "50px" }}
+          />
+          <img src={logo} style={{ height: "100px" }} />
+        </Row>
+        <Row horizontal="center">
+          <h1>Target QR Code Creator</h1>
+        </Row>
+        <Row horizontal="center">
+          <CSVReader
+            cssClass="react-csv-input"
+            label="Choose a file "
+            onFileLoaded={this.csvUpload}
+          />
+        </Row>
+      </Column>
+    );
+  }
 }
 
 export default App;
