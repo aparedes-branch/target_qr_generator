fd2f61aebc123b007533c1f3259f8b203f657e53
Aloysius Paredes <aloysius.paredes@gmail.com>
2020-01-03 12:14:53 -0600

    Updates

---
+++ precache-manifest.df192934bf79dcd6c55b356b0b6765e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb42e7c0cc79aa9dc31043ca1e941236",
+    "url": "/target_qr_generator/index.html"
+  },
+  {
+    "revision": "f38e1cde74bed734c55d",
+    "url": "/target_qr_generator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f887c7d1e6d0d4426cf5",
+    "url": "/target_qr_generator/static/js/2.4f26dfce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/target_qr_generator/static/js/2.4f26dfce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f38e1cde74bed734c55d",
+    "url": "/target_qr_generator/static/js/main.2d31eda3.chunk.js"
+  },
+  {
+    "revision": "729683d06dcec6727f5f",
+    "url": "/target_qr_generator/static/js/runtime-main.9bd499d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/target_qr_generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
